2360aaad2a4c2263e4f4eb46e4fedb89edd63beb
Benito <bbxblack@gmail.com>
2021-05-06 16:44:07 +0200

    v0.1

---
+++ src/script.js
@@ -0,0 +1,27 @@
+function init() {
+    new Vue({
+        el: "#app",
+        data: {
+            'hello': 'Hello World',
+            disk: [],
+            // dskPoster: '',
+            // dskTitle: '',
+            // dskGenre: '',
+            // dskAuthor: '',
+            // dskYear: ''
+        },
+        mounted() {
+            axios.get('data.php')
+                .then(r => {
+                    console.log(r.data);
+                    this.disk = r.data
+
+
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded",init);
